83d3fb3f9431642da54502a45dfd9d3d9b0c861b
zhangyufeng004 <zhangyufeng004@ke.com>
2022-04-22 14:03:43 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "../src/assest/common/reset.scss";
+import Home from "../src/pages/Home";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div className="content">
+        <Switch>
+          <Route path="/" component={Home} />
+          {/* <Route path="/about" component={App} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+function Home() {
+  return (
+    <div className="page-home-wrap">
+      <span className="title">我是demo</span>
+    </div>
+  );
+}
+
+export default Home;
